ab09cf51476cae8e93a9e957f874f1044fe13e6d
lucacappello79 <postt79@gmail.com>
2023-02-21 16:56:40 +0100

    feat: added compact version, added PW-only version

--- js/script.js
+++ js/script.js
@@ -7,19 +7,32 @@
 // - memorizzarlo in una variabile
 // - concatenarli e stamparli in pagina
 
-let userName;
-let userLastName;
-let userFavColor;
+// --------- Raggruppato su una riga
+
+let userName = prompt("Inserisci il tuo nome")
+let userLastName = prompt("Inserisci il tuo cognome")
+let userFavColor = prompt("Inserisci il tuo colore preferito")
+
+
+// --------- Suddiviso in più righe
+
+// let userName;
+// let userLastName;
+// let userFavColor;
+
 // let userLuckyNumber;
 
-userName = prompt("Inserisci il tuo nome");
+// userName = prompt("Inserisci il tuo nome");
 
-userLastName = prompt("Inserisci il tuo cognome");
+// userLastName = prompt("Inserisci il tuo cognome");
 
-userFavColor = prompt("Inserisci il tuo colore preferito");
+// userFavColor = prompt("Inserisci il tuo colore preferito");
 
 // userLuckyNumber = prompt("Inserisci il tuo numero fortunato")
 
 document.writeln("Ciao " + userName + " " + userLastName + ". Il tuo colore preferito è il " + userFavColor + ". Il tuo numero fortunato è " + (10 - 2 * 10 / 2 + 1));
 
 console.log(`TEMPLATE LITERALS VERSION: Ciao ${userName} ${userLastName}. Il tuo colore preferito è il ${userFavColor}. Il tuo numero fortunato è ${10 - 2 * 10 / 2 + 1}`)
+
+// --------- Versione solo PW
+// document.writeln(userName + userLastName + userFavColor + 47);
