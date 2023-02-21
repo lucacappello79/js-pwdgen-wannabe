0d4c570965858448b6a4ad5c49d5e206068bbcf0
lucacappello79 <postt79@gmail.com>
2023-02-21 13:47:34 +0100

    feat: added variations

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
 
+// - chiedere all'utente il suo nome
+// - memorizzarlo in una variabile
+// - chiedere all'utente il suo cognome
+// - memorizzarlo in una variabile
+// - chiedere all'utente il suo colore preferito
+// - memorizzarlo in una variabile
+// - concatenarli e stamparli in pagina
+
 let userName;
 let userLastName;
 let userFavColor;
+// let userLuckyNumber;
 
 userName = prompt("Inserisci il tuo nome");
 
@@ -9,4 +18,8 @@ userLastName = prompt("Inserisci il tuo cognome");
 
 userFavColor = prompt("Inserisci il tuo colore preferito");
 
+// userLuckyNumber = prompt("Inserisci il tuo numero fortunato")
+
 document.writeln("Ciao " + userName + " " + userLastName + ". Il tuo colore preferito è il " + userFavColor + ". Il tuo numero fortunato è " + (10 - 2 * 10 / 2 + 1));
+
+console.log(`TEMPLATE LITERALS VERSION: Ciao ${userName} ${userLastName}. Il tuo colore preferito è il ${userFavColor}. Il tuo numero fortunato è ${10 - 2 * 10 / 2 + 1}`)
