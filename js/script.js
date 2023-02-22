adb099d5317ba47c8f45f4366a7d2438ecad1afe
lucacappello79 <postt79@gmail.com>
2023-02-22 09:56:56 +0100

    feat: added ;

--- js/script.js
+++ js/script.js
@@ -9,9 +9,9 @@
 
 // --------- Raggruppato su una riga
 
-let userName = prompt("Inserisci il tuo nome")
-let userLastName = prompt("Inserisci il tuo cognome")
-let userFavColor = prompt("Inserisci il tuo colore preferito")
+let userName = prompt("Inserisci il tuo nome");
+let userLastName = prompt("Inserisci il tuo cognome");
+let userFavColor = prompt("Inserisci il tuo colore preferito");
 
 
 // --------- Suddiviso in più righe
@@ -28,11 +28,12 @@ let userFavColor = prompt("Inserisci il tuo colore preferito")
 
 // userFavColor = prompt("Inserisci il tuo colore preferito");
 
-// userLuckyNumber = prompt("Inserisci il tuo numero fortunato")
+// userLuckyNumber = prompt("Inserisci il tuo numero fortunato");
 
 document.writeln("Ciao " + userName + " " + userLastName + ". Il tuo colore preferito è il " + userFavColor + ". Il tuo numero fortunato è " + (10 - 2 * 10 / 2 + 1));
 
-console.log(`TEMPLATE LITERALS VERSION: Ciao ${userName} ${userLastName}. Il tuo colore preferito è il ${userFavColor}. Il tuo numero fortunato è ${10 - 2 * 10 / 2 + 1}`)
+console.log(`TEMPLATE LITERALS VERSION: Ciao ${userName} ${userLastName}. Il tuo colore preferito è il ${userFavColor}. Il tuo numero fortunato è ${10 - 2 * 10 / 2 + 1}`);
 
 // --------- Versione solo PW
 // document.writeln(userName + userLastName + userFavColor + 47);
+
